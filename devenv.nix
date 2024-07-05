90aa4c540621b77ff5a7c76d3ff641ff3848c5f6
Wayne Duran <asartalo@gmail.com>
2024-07-05 16:42:38 +0800

    first commit

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "ASSG - Asartalo’s Static Site Generator";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ nodejs_18 ];
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo $GREET";
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    go test
+  '';
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/languages/
+  languages.go.enable = true;
+
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs_18;
+  };
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
