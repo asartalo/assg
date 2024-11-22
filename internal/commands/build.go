6bf75db438aa118f30429dc60b2339f2d3a87c29
Wayne Duran <asartalo@gmail.com>
2024-11-22 15:13:17 +0800

    refactor: cleaning up unnecessary type declaration on build command source

--- internal/commands/build.go
+++ internal/commands/build.go
@@ -1,19 +1,12 @@
 package commands
 
 import (
-	"html/template"
 	"path"
 	"time"
 
 	"github.com/asartalo/assg/internal/config"
-	"github.com/asartalo/assg/internal/content"
 	"github.com/asartalo/assg/internal/generator"
 )
-
-type TemplateContent struct {
-	content.FrontMatter
-	Content template.HTML
-}
 
 func Build(srcDir, outputDir string, includeDrafts bool, verbose bool, now time.Time) error {
 	config, err := config.Load(path.Join(srcDir, "config.toml"))
