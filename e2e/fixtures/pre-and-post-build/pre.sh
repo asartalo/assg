157dd995c8dce76d2388a81a22f83c272e66eb5d
Wayne Duran <asartalo@gmail.com>
2024-12-10 14:30:45 +0800

    chore: try more output on build scripts

--- e2e/fixtures/pre-and-post-build/pre.sh
+++ e2e/fixtures/pre-and-post-build/pre.sh
@@ -1,6 +1,8 @@
 cd $ASSG_ROOT
 echo "HELLO FROM pre.sh"
 echo "ASSG_ROOT: $ASSG_ROOT"
+pwd
+ls -l 
 if [ -f ./content/pre.txt ]; then
     rm ./content/pre.txt
 fi
@@ -8,4 +10,5 @@ if [ -f ./content/post.txt ]; then
     rm ./content/post.txt
 fi
 foo=`ls ./content`
+echo $foo
 echo $foo > ./content/pre.txt
